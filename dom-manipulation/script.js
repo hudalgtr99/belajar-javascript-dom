7b73d45417f4e0a6a21fc208487ebe404eb8e072
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-10 15:22:59 +0700

    Update Syntax error

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,6 +1,12 @@
+// // element.innerHTML
 // const judul = document.getElementById('judul');
 // judul.innerHTML = '<em> Miftahul Huda Guntara </em>';
 
 // const sectionA = document.querySelector('section#a');
 // sectionA.innerHTML = 'hello world'; //Mengubah seluruh section A hanya dengan hello world
 
+
+// // element.style.<property>
+const judul = document.querySelector('#judul');
+judul.style.color = 'lightblue';
+judul.style.backgroundCOlor = 'salmon';
