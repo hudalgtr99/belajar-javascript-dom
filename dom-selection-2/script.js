844ca57f7652db7f3072d4ae92f1672efb9fd2d4
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-10 14:05:21 +0700

    Update query selector

--- dom-selection-2/script.js
+++ dom-selection-2/script.js
@@ -4,3 +4,14 @@
 // document.getElementsByClassName()
 
 // document.querySelector()
+
+const p4 = document.querySelector('#b p');
+p4.style.color = 'green';
+p4.style.fontSize = '30px';
+
+const li2 = document.querySelector('section#b ul li:nth-child(2)');
+li2.style.backgroundColor = 'orange';
+
+// Hanya paragraf/p yg pertama kali ditemukan yang diubah
+const p = document.querySelector('p');
+p.innerHTML = 'Ini diubah melalui Javascript';
