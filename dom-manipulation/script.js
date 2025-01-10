e68685f2f8c735ce8f08c28d2c2f03b29f991516
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-10 15:19:57 +0700

    Comment all sementara

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,3 +1,6 @@
-const judul = document.getElementById('judul');
-judul.innerHTML = '<em> Miftahul Huda Guntara </em>';
+// const judul = document.getElementById('judul');
+// judul.innerHTML = '<em> Miftahul Huda Guntara </em>';
+
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world'; //Mengubah seluruh section A hanya dengan hello world
 
