53232574383c83b47ed4041ed5894e5b04ac3c7c
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-10 11:00:01 +0700

    Update background Color

--- dom-selection/script.js
+++ dom-selection/script.js
@@ -2,3 +2,9 @@
 // document.getElementById()
 const judul = document.getElementById('judul');
 judul.style.color = 'cyan';
+
+// Di javascript jangan gunakan strip untuk
+// 2 kata seperti background-color. Tapi gunakan
+// camelCase menjadi backgroundColor.
+judul.style.backgroundColor = '#ccc';
+
