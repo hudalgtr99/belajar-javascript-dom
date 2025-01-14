179f81ad13918951ab867f4f2affb5ab4faad5e5
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-14 11:12:03 +0700

    Membuat method pada p2 dijavascript

--- dom-event/script.js
+++ dom-event/script.js
@@ -3,3 +3,7 @@ const p3 = document.querySelector('.p3');
 function ubahWarna(){
     p3.style.backgroundColor = 'lightblue';
 }
+
+const p2 = document.querySelector('.p2');
+p2.onclick = ubahWarna; // ubahWarna tidak perlu menggunakan kurung agar tidak langsung dirun ketika belum diklik
+
