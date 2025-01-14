616131a18d3bab29daadae23e9a95d90f33cbabf
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-14 13:12:54 +0700

    Comment all code

--- dom-event/script.js
+++ dom-event/script.js
@@ -1,23 +1,25 @@
-const p3 = document.querySelector('.p3');
+// const p3 = document.querySelector('.p3');
 
-function ubahWarnaP2(){
-    p2.style.backgroundColor = 'lightblue';
-}
+// function ubahWarnaP2(){
+//     p2.style.backgroundColor = 'lightblue';
+// }
 
-function ubahWarnaP3(){
-    p3.style.backgroundColor = 'lightblue';
-}
+// function ubahWarnaP3(){
+//     p3.style.backgroundColor = 'lightblue';
+// }
 
-const p2 = document.querySelector('.p2');
-p2.onclick = ubahWarnaP2; // ubahWarna tidak perlu menggunakan kurung agar tidak langsung dirun ketika belum diklik
+// const p2 = document.querySelector('.p2');
+// p2.onclick = ubahWarnaP2; // ubahWarna tidak perlu menggunakan kurung agar tidak langsung dirun ketika belum diklik
 
 
-// // addEventListener()
-const p4 = document.querySelector('section#b p');
-p4.addEventListener('click', function(){
-    const ul = document.querySelector('section#b ul');
-    const liBaru = document.createElement('li');
-    const teksLiBaru = document.createTextNode('item baru');
-    liBaru.appendChild(teksLiBaru);
-    ul.appendChild(liBaru);
-})
+// // // addEventListener()
+// const p4 = document.querySelector('section#b p');
+// p4.addEventListener('click', function(){
+//     const ul = document.querySelector('section#b ul');
+//     const liBaru = document.createElement('li');
+//     const teksLiBaru = document.createTextNode('item baru');
+//     liBaru.appendChild(teksLiBaru);
+//     ul.appendChild(liBaru);
+// })
+
+
