2dd8085c4075732552283f6292e8c1691325a82f
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-14 15:15:33 +0700

    Comment all code

---
+++ dom-traversal/script.js
@@ -0,0 +1,12 @@
+// const close = document.querySelector('.close');
+
+// const card = document.querySelector('.card');
+
+// close.addEventListener('click', function(){
+//     // card.remove(); //Menghapus
+//     card.style.display = 'none'; //menutup
+// });
+
+
+
+//// DOM Traversal
