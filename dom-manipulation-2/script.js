6816108f873f137e7bff92a1b2cb5c9314ea667d
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-14 09:23:18 +0700

    Update Comment

--- dom-manipulation-2/script.js
+++ dom-manipulation-2/script.js
@@ -1,5 +1,7 @@
 // DOM manipulation
 
+// // appendChild = memasukkan node/child di akhir parentnya
+
 // Buat elemen baru
 const pBaru = document.createElement('p');
 const teksPBaru = document.createTextNode('Paragraf Baru');
@@ -10,3 +12,5 @@ pBaru.appendChild(teksPBaru);
 //Simpan pBaru di akhir Section A
 const sectionA = document.getElementById('a');
 sectionA.appendChild(pBaru);
+
+// // insertBefore
