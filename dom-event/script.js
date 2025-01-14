05177bb95d0da9b8568ca329b2fee127b69107f2
hudalgtr99 <hudalgtr99@gmail.com>
2025-01-14 11:34:44 +0700

    Menyelesaikan AddEventListener pada p4

--- dom-event/script.js
+++ dom-event/script.js
@@ -11,3 +11,13 @@ function ubahWarnaP3(){
 const p2 = document.querySelector('.p2');
 p2.onclick = ubahWarnaP2; // ubahWarna tidak perlu menggunakan kurung agar tidak langsung dirun ketika belum diklik
 
+
+// // addEventListener()
+const p4 = document.querySelector('section#b p');
+p4.addEventListener('click', function(){
+    const ul = document.querySelector('section#b ul');
+    const liBaru = document.createElement('li');
+    const teksLiBaru = document.createTextNode('item baru');
+    liBaru.appendChild(teksLiBaru);
+    ul.appendChild(liBaru);
+})
